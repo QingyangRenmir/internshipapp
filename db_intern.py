fde3c02791eb290d75967834d8db4e698e9c3b1b
Qingyang Ren <74858248+QingyangRenmir@users.noreply.github.com>
2021-06-12 14:58:43 +0800

    Add files via upload
    database and api

---
+++ db_intern.py
@@ -0,0 +1,161 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy() #db is an instance of sqlalchemy
+
+association_table1 = db.Table(
+	"association_table1",
+	db.Model.metadata,
+	db.Column("employee_id", db.Integer, db.ForeignKey("employee.id")),
+	db.Column("employer_id", db.Integer, db.ForeignKey("employer.id"))
+)
+
+# association_table2 = db.Table(
+# 	"association_table2",
+# 	db.Model.metadata,
+# 	db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
+#     db.Column("student_id", db.Integer, db.ForeignKey("user.id"))
+# )
+
+# your classes here
+class Employee(db.Model):
+    __tablename__ = 'employee' #same name as py class
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    gpa = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    picture = db.Column(db.String, nullable=False)
+    #assignments = db.relationship('Assignment', cascade = 'delete') #when delete task go ahead to delete subtasks
+    #employers = db.relationship('Employer', secondary = association_table1, back_populates='employees')
+    # students = db.relationship('User', secondary = association_table2, back_populates='courses_s')
+    job_accepted = db.Column(db.Integer, nullable=False) # not using foreign key, because want to use -1 to represent None
+    past_work_experience = db.Column(db.String)
+    start_month = db.Column(db.Integer, nullable=False)
+    end_month = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String)
+
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', "")
+        self.gpa = kwargs.get('gpa', 0)
+        self.location = kwargs.get('location', "")
+        self.picture = kwargs.get("picture", "")
+        self.past_work_experience = kwargs.get("past_work_experience", "")
+        self.start_month = kwargs.get("start_month", 0)
+        self.end_month = kwargs.get("end_month", 0)
+        self.email = kwargs.get("email", "")
+        self.job_accepted = -1
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'name':self.name,
+            'location': self.location,
+            "picture": self.picture,
+            "gpa": self.gpa,
+            "past_work_experience": self.past_work_experience,
+            "start_month": self.start_month,
+            "end_month": self.end_month,
+            "job_accepted": "None" if self.job_accepted == -1 else Joboffer.query.filter_by(id = self.job_accepted).first().title,
+            "prefered_position": [t.title for t in PreferedPosition.query.filter_by(employee_id=self.id).all()],
+            "email": self.email
+        }
+
+    # def serialize2(self):
+    #     return{
+    #         'id': self.id,
+    #         'code': self.code,
+    #         'name': self.name,
+    #         'assignments': [],
+    #         'instructors':[],
+    #         'students': [],
+    #     }
+class PreferedPosition(db.Model):
+    __tablename__ = 'preferedposition'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    employer_id = db.Column(db.Integer, db.ForeignKey('employer.id'), nullable=False)
+    start_month = db.Column(db.Integer, nullable=False)
+    end_month = db.Column(db.Integer, nullable=False)
+    open_slots = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, **kwargs):
+        self.title = kwargs.get('title', '')
+        self.employee_id = kwargs.get('employee_id')
+        self.employer_id = kwargs.get('employer_id')
+        self.start_month = kwargs.get("start_month", 0)
+        self.end_month = kwargs.get("end_month", 0)
+        self.open_slots = kwargs.ger("open_slots", 0)
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'title': self.title,
+            'employee_id': self.employee_id,
+            'employer_id': self.employer_id,
+	        'start_month': self.start_month,
+	        'end_month': self.end_month,
+	        'open_slots': self.open_slots,
+        }
+
+class Employer(db.Model):
+    __tablename__ = 'employer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    picture = db.Column(db.String, nullable=False)
+    # employees = db.relationship('Employee', secondary = association_table2, back_populates='employers')
+    # courses_i = db.relationship('Course', secondary = association_table1, back_populates='instructors')
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', '')
+        self.location = kwargs.get('netid', "")
+        self.picture = kwargs.get("picture", "")
+        # self.employee_id = kwargs.get('employee_id')
+
+    def serialize(self):
+        return{
+        'id': self.id,
+        'name': self.name,
+        'location':self.location,
+        "picture": self.picture,
+        "job_offer": [t.title for t in Joboffer.query.filter_by(employer_id=self.id).all()]
+    }
+
+    # def serialize2(self):
+    #     course = []
+    #     for c in self.courses_s:
+    #         course.append(c.serialize2())
+    #     for c in self.courses_i:
+    #         course.append(c.serialize2())
+    #     return{
+    #         'id': self.id,
+    #         'name': self.name,
+    #         'netid': self.netid,
+    #         'courses': course,
+    #     }
+
+class Joboffer(db.Model):
+    __tablename__ = 'joboffer'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    employer_id = db.Column(db.Integer, db.ForeignKey('employer.id'), nullable=False)
+    start_month = db.Column(db.Integer, nullable=False)
+    end_month = db.Column(db.Integer, nullable=False)
+    open_slots = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, **kwargs):
+        self.title = kwargs.get('title', '')
+        self.employer_id = kwargs.get('employer_id')
+        self.start_month = kwargs.get("start_month", 0)
+        self.end_month = kwargs.get("end_month", 0)
+        self.open_slots = kwargs.ger("open_slots", 0)
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'title': self.title,
+            'employer_id': self.employer_id,
+	    	'start_month': self.start_month,
+	    	'end_month': self.end_month,
+	    	'open_slots': self.open_slots,
+        }
---
+++ internship_app.py
@@ -0,0 +1,148 @@
+from flask import Flask, request
+from db_intern import db
+from db_intern import Employee, Employer, Joboffer, PreferedPosition
+import json
+import os
+
+app = Flask(__name__)
+db_filename = "data.db"
+default_picture = "someLink.jpg"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+#initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+#generalized response formats
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}), code
+
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
+
+#@app.route("/")
+@app.route("/api/employees/")
+def get_employees():
+    return success_response([t.serialize() for t in Employee.query.all()])
+
+@app.route("/api/employees/", methods=["POST"])
+def create_employees():
+    body = json.loads(request.data)
+    name = body.get('name')
+    gpa = body.get('gpa')
+    location = body.get('location')
+    picture = body.get("picture", default_picture)
+    past_work_experience = body.get("past_work_experience", "")
+    start_month = body.get("start_month")
+    end_month = body.get("end_month")
+    email = body.get("email")
+    if name is None or gpa is None or location is None or start_month is None or end_month is None:
+        return failure_response("Incomplete field!")
+    new_employee = Employee(name = name, gpa = gpa, location = location, picture = picture, past_work_experience = past_work_experience, start_month = start_month, end_month = end_month, email = email)
+    print("new_employee:",new_employee)
+    db.session.add(new_employee)
+    db.session.commit()
+    return success_response(new_employee.serialize(), 201)
+
+
+@app.route("/api/employees/<int:employee_id>/", methods=["DELETE"])
+def delete_employee(employee_id):
+    employee = Employee.query.filter_by(id=employee_id).first()
+    if employee is None:
+        return failure_response('Employee not found!')
+    db.session.delete(employee)
+    db.session.commit()
+    return success_response(employee.serialize())
+
+@app.route("/api/employers/", methods=["POST"])
+def create_employer():
+    body = json.loads(request.data)
+    name = body.get('name')
+    location = body.get('location')
+    picture = body.get("picture", default_picture)
+    if name is None or location is None :
+        return failure_response("Incomplete field!")
+    new_employer = Employer(name=name, location=location, picture = picture)
+    db.session.add(new_employer)
+    db.session.commit()
+    return success_response(new_employer.serialize(), 201)
+
+@app.route("/api/employers/<int:employer_id>/")
+def get_employer(employer_id):
+    employer = Employer.query.filter_by(id=employer_id).first()
+    if employer is None:
+        return failure_response('Employer not found!')
+    return success_response(employer.serialize())
+
+@app.route("/api/employees/<int:employee_id>/acceptOffer/", methods=["POST"])
+def add_employer():
+    employee = Employee.query.filter_by(id=employee_id).first()
+    if employee is None:
+        return failure_response('Employee not found!')
+    body = json.loads(request.data)
+    job_offer_id=body.get('job_offer_id')
+    job_offer =  Joboffer.query.filter_by(id=job_offer_id).first()
+    if job_offer is None:
+        return failure_response('Employer not found!')
+    # if body.get('type') is None:
+    #     return failure_response('Type not found!')
+    # if body.get('type') == "student":
+    #     course.students.append(user)
+    #     user.courses_s.append(course)
+    # elif body.get('type') == "instructor":
+    #     course.students.append(user)
+    #     user.courses_i.append(course)
+    #     print("instructor")
+    # else:
+    #    return failure_response("Invalid type!")
+    if job_offer.open_slots < 1:
+        return failure_response("Job offer unavailable")
+    employee.job_accepted = job_offer_id
+    job_offer.open_slots -= 1
+    db.session.commit()
+    return success_response(employee.serialize())
+
+@app.route("/api/job_offers/", methods=["POST"])
+def create_job_offer():
+    body = json.loads(request.data)
+    title = body.get('title')
+    employer_id = body.get('employer_id')
+    start_month = body.get("start_month")
+    end_month = body.get("end_month")
+    employer = Employer.query.filter_by(id=employer_id).first()
+    if name is None or start_month is None or end_month is None:
+        return failure_response("Incomplete field!")
+    if employer is None:
+        return failure_response("Employer not found")
+    new_job_offer = Joboffer(title=title, employer_id=employer_id, start_month = start_month, end_month = end_month)
+    db.session.add(new_job_offer)
+    db.session.commit()
+    return success_response(new_job_offer.serialize())
+#assignment routes
+
+# @app.route("/api/courses/<int:course_id>/assignment/", methods=["POST"])
+# def add_assignment(course_id):
+#     course = Course.query.filter_by(id=course_id).first()
+#     if course is None:
+#         return failure_response('Course not found!')
+#     body = json.loads(request.data)
+#     title = body.get('title')
+#     due_date = body.get('due_date')
+
+#     if title is None or due_date is None:
+#         return failure_response("Incomplete field!")
+#     new_assignment = Assignment(title=title, due_date=due_date, course_id=course_id)
+#     db.session.add(new_assignment)
+#     db.session.commit()
+#     response = new_assignment.serialize()
+#     response["course"] = Course.query.filter_by(id = course_id).first().serialize()
+#     return success_response(response)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
